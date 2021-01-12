a2239b3771d551256828291c2d85d44b27e1ef3e
GriffinSauce <jorisjgriffioen@gmail.com>
2021-01-12 21:21:38 +0100

    Figure out issue root and move into testSetup

--- src/components/Navigation.test.tsx
+++ src/components/Navigation.test.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import { render, act } from 'test-utils';
+import { render } from 'test-utils';
 import { axe } from 'jest-axe';
 import Navigation from './Navigation';
 
 it('passes accessibility tests', async () => {
   const { container } = render(<Navigation />);
-
-  // TODO: act() is needed here to prevent test log noise
-  // https://github.com/nickcolley/jest-axe/issues/95
-  let results;
-  await act(async () => {
-    results = await axe(container);
-  });
+  const results = await axe(container);
 
   expect(results).toHaveNoViolations();
 });
--- test/test.setup.ts
+++ test/test.setup.ts
@@ -1,8 +1,11 @@
 import '@testing-library/jest-dom';
 import 'jest-axe/extend-expect';
 
-// TODO: remove this when issue is fixed
-// jsdom doesn't support the second param and adds test log noise
-// https://github.com/nickcolley/jest-axe/issues/147
+// JSDom doesn't support the second param and adds test log noise
+// TODO: clean up when fixed: https://github.com/nickcolley/jest-axe/issues/147
 const { getComputedStyle } = window;
 window.getComputedStyle = (elt) => getComputedStyle(elt);
+
+// Prevent act() error logs
+// TODO: clean up when fixed: https://github.com/vercel/next.js/pull/20169
+jest.mock('next/link', () => ({ children }) => children);
